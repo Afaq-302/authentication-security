c471f1c3f68c381ef746f803a6c2c8fbe436ddb4
Afaq-302 <ufaq302@yahoo.com>
2023-02-05 20:29:13 +0500

    Updated google

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ passport.deserializeUser(function (user, cb) {
 
 // Google Auth
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: "http://localhost:3000/auth/google/secrets",
     callbackURL: "https://secrets-app-mn1w.onrender.com/auth/google/secrets"
     // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
